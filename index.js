3bf0075b25da3ec5468db07e5db7e067b268036a
amit00978 <amit00978@gmail.com>
2019-06-18 15:32:04 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+
+import BgTracking from './src/BgTracking';
+
+AppRegistry.registerComponent('RNBGExample', () => BgTracking);
